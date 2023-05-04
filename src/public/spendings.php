0509f00ffe5ad7018a5f2e31cb20405dd5183baa
mikan19 <mellamo.rina.ty@gmail.com>
2023-05-04 17:02:20 +0900

    feat: amountカラムの合計の計算をクラス化しました

---
+++ src/public/spendings.php
@@ -0,0 +1,43 @@
+<?php
+
+class Spendings {
+  private $pdo;
+
+  function __construct($dbUserName, $dbPassword, $dbName) {
+    $dsn = "mysql:host=mysql; dbname=$dbName; charset=utf8";
+    $this->pdo = new PDO($dsn, $dbUserName, $dbPassword);
+  }
+
+  function getTotalSpendingsAmount() {
+    $sql = "SELECT * FROM spendings";
+    $statement = $this->pdo->prepare($sql);
+    $statement->execute();
+    $spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $totalSpendingsAmount = 0;
+    foreach ($spendings as $spending) {
+      $totalSpendingsAmount += $spending['amount'];
+    }
+
+    return $totalSpendingsAmount;
+  }
+}
+
+$spendingTracker = new Spendings ("root", "password", "tq_quest");
+$totalSpendingsAmount = $spendingTracker->getTotalSpendingsAmount();
+echo $totalSpendingsAmount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
